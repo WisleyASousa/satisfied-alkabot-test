52fba6c3c188451fcb27dba7a2d016502686e5e4
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-03-27 09:33:01 -0300

    add um visual melhor, e add uma posivel resposta na pagina status

---
+++ src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import MainLayout from '../layout/MainLayout'
+
+const Profile: NextPage = () => {
+  return (
+    <MainLayout title="Satisfied">
+      <div className=""></div>
+    </MainLayout>
+  )
+}
+
+export default Profile
